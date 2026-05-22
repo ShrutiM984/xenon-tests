f72e228a989c9c13328d9cd36bc56213d5ac7d93
ShrutiM984 <shruti.mamidwar@jadeglobal.com>
2026-05-23 00:03:27 +0530

    Decrease timeout for 'New' button selector
    Reduced timeout for waiting on the 'New' button to 1000ms.

--- automation-scripts/Playwright/JavaScript/tc033.spec.js
+++ automation-scripts/Playwright/JavaScript/tc033.spec.js
@@ -6,7 +6,7 @@ test('FORCE timeout on New button', async ({ page }) => {
 
   await page.locator('a[title="Leads"]').click();
 
-  await page.waitForSelector('button[name="New"]', { timeout: 2000 });
+  await page.waitForSelector('button[name="New"]', { timeout: 1000 });
 
   await page.click('button[name="New"]');
 });
