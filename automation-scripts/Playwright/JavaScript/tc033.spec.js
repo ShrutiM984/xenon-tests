45d01d5ea63f30e1883fc76268adba1d77d97512
ShrutiM984 <shruti.mamidwar@jadeglobal.com>
2026-05-22 23:28:22 +0530

    Update URL for page navigation in test case

--- automation-scripts/Playwright/JavaScript/tc033.spec.js
+++ automation-scripts/Playwright/JavaScript/tc033.spec.js
@@ -2,7 +2,7 @@ const { test } = require('@playwright/test');
 
 test('FORCE timeout on New button', async ({ page }) => {
 
-  await page.goto('https://login.salesforce.com');
+  await page.goto('https://orgfarm-33b82f167b-dev-ed.develop.lightning.force.com/lightning/page/home');
 
   await page.locator('a[title="Leads"]').click();
 
