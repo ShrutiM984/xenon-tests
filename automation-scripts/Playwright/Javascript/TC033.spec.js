0e5eb20aaf681e29c30a6ce659f4e366503dfc3b
ShrutiM984 <shruti.mamidwar@jadeglobal.com>
2026-05-23 18:15:19 +0530

    Self-healed test script: TC033

---
+++ automation-scripts/Playwright/Javascript/TC033.spec.js
@@ -0,0 +1,12 @@
+const { test } = require('@playwright/test');
+
+test('FORCE timeout on New button', async ({ page }) => {
+
+  await page.goto('https://orgfarm-33b82f167b-dev-ed.develop.lightning.force.com/lightning/page/home');
+
+  await page.locator('a[title="Leads"]').click();
+
+  await page.waitForSelector('button[name="New"]', { timeout: 15000 });
+
+  await page.click('button[name="New"]');
+});
