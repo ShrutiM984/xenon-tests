977a0a83c81b26f2934d43ec19bad75bfc326f2e
ShrutiM984 <shruti.mamidwar@jadeglobal.com>
2026-05-23 15:26:26 +0530

    Update tc040.spec.js

--- automation-scripts/Playwright/JavaScript/tc040.spec.js
+++ automation-scripts/Playwright/JavaScript/tc040.spec.js
@@ -1,35 +1,27 @@
-const { test, expect } = require('@playwright/test');
+await page.goto(
+  'https://enterprise-app-2429.lightning.force.com/lightning/o/Account/list?filterName=AllAccounts'
+);
 
-test.describe('Salesforce Lead Creation', () => {
+await page.waitForLoadState('domcontentloaded');
 
-  test('Verify successful Lead creation', async ({ page }) => {
+// Click Accounts tab
+const accountsTab = page.getByRole('link', { name: 'Accounts' });
 
-    test.setTimeout(120000);
-
-    await page.goto(
-      'https://enterprise-app-2429.lightning.force.com/lightning/o/Lead/list?filterName=AllOpenLeads'
-    );
-
-    await page.waitForLoadState('domcontentloaded');
-
-   await page.waitForSelector('button[name="New"]', {
-      timeout: 15000
-    });
-
-    await page.click('button[name="New"]');
-
-    const lastName = `Lead_${Date.now()}`;
-
-    await page.fill('input[name="lastName"]', lastName);
-
-    await page.fill('input[name="Company"]', 'Xenon AI');
-
-    await page.click('button[name="SaveEdit"]');
-
-    const toast = page.locator('span.toastMessage');
-
-    await expect(toast).toContainText('Lead');
-
-  });
-
+await accountsTab.waitFor({
+  state: 'visible',
+  timeout: 30000
 });
+
+await accountsTab.click();
+
+// Click New button
+const newButton = page.locator(
+  'li[data-target-selection-name="sfdc:StandardButton.Account.New"] a'
+);
+
+await newButton.waitFor({
+  state: 'visible',
+  timeout: 30000
+});
+
+await newButton.click();
